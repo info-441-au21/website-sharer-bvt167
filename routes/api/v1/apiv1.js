860949a18fa9791df03bcacbbd54bf16da7b61a4
bvt <bvt@uw.edu>
2021-10-19 14:34:25 -0700

    finished main requirements, need to do creative component

--- routes/api/v1/apiv1.js
+++ routes/api/v1/apiv1.js
@@ -5,6 +5,14 @@ var router = express.Router();
 
 router.get('/previewurl', function(req, res, next) {
   const url = req.query.url;
+  if (url === "http://localhost:3000/") {
+    const previewHtmlObj = {
+      "url": url,
+      "title": "Brandon Ta",
+    }
+    res.send(getPreviewHtml(previewHtmlObj));
+    return;
+  }
   const SELECTED_META_TAGS = new Set(["og:url", "og:title", "og:image", "og:description"])
   fetch(url)
     .then(resp => resp.text())
@@ -12,10 +20,10 @@ router.get('/previewurl', function(req, res, next) {
       const parsedHtml = parse(respText);
       const metaTags = parsedHtml.querySelectorAll('meta').map(tag => tag.attributes);
       const filteredMetaTags  = metaTags.filter(tag => SELECTED_META_TAGS.has(tag.property));
-      const metaTagsObj = filteredMetaTags.
-        reduce((obj, item) => (obj[item.property] = item.content, obj) ,{});
+      const metaTagsObj = filteredMetaTags.reduce((obj, item) => (obj[item.property] = item.content, obj), {});   // Convert array of elements to an object
+      console.dir(metaTagsObj);
       const previewHtmlObj = getPreviewHtmlObj(metaTagsObj, parsedHtml, url);
-      res.send(parsedHtml.toString());
+      res.send(getPreviewHtml(previewHtmlObj));
     })
     .catch(e => res.send(e));
 });
@@ -23,6 +31,27 @@ router.get('/previewurl', function(req, res, next) {
 const getPreviewHtmlObj = (metaTagsObj, parsedHtml, url) => {
   let previewHtmlObj = getEmptyPreviewHtmlObj();
   previewHtmlObj["url"] = metaTagsObj["og:url"] ? metaTagsObj["og:url"] : url;
+  previewHtmlObj["title"] = metaTagsObj["og:title"] ? metaTagsObj["og:title"] : parsedHtml.querySelectorAll("title")[0].textContent;
+  previewHtmlObj["image"] = metaTagsObj["og:image"];
+  previewHtmlObj["description"] = metaTagsObj["og:description"];
+  if (!previewHtmlObj["title"]) {
+    previewHtmlObj["title"] = url;
+  }
+  return previewHtmlObj;
+}
+
+const getPreviewHtml = (previewHtmlObj) => {
+  return `
+    <div style="max-width: 300px; border: solid 1px; padding: 3px; text-align: center;">
+      <a href="${previewHtmlObj["url"]}">
+          <p><strong>
+            ${previewHtmlObj["title"]}
+          </strong></p>
+          ${previewHtmlObj["image"] ? `<img src="${previewHtmlObj["image"]}" style="max-height: 200px; max-width: 270px;">` : ""}
+      </a>
+      ${previewHtmlObj["description"] ? `<p>${previewHtmlObj["description"]}</p>` : ""}
+    </div>
+  `;
 }
 
 const getEmptyPreviewHtmlObj = () => {
